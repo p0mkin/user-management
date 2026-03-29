062d172c7fd518d501cbb31c15383122ca23f358
p0mkin <77664722+p0mkin@users.noreply.github.com>
2026-03-29 18:04:28 +0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($class) {
-    // Remove "App\" prefix because your folders are in the root directory
+
     $classPath = str_replace('App\\', '', $class);
     
     $file = __DIR__ . '/' . str_replace('\\', '/', $classPath) . '.php';
